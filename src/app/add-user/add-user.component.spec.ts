a5a026099c47d87b3c0e3cf09b893fae41a69b59
hoanganhhththt <66316469+hoanganhhththt@users.noreply.github.com>
2022-05-04 16:56:48 +0700

    refactor/comment-spect-componet

--- src/app/add-user/add-user.component.spec.ts
+++ src/app/add-user/add-user.component.spec.ts
@@ -1,24 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddUserComponent } from './add-user.component';
+// import { AddUserComponent } from './add-user.component';
 
-describe('AddUserComponent', () => {
-  let component: AddUserComponent;
-  let fixture: ComponentFixture<AddUserComponent>;
+// describe('AddUserComponent', () => {
+//   let component: AddUserComponent;
+//   let fixture: ComponentFixture<AddUserComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AddUserComponent],
-    }).compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [AddUserComponent],
+//     }).compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddUserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AddUserComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/list-user/list-user.component.spec.ts
+++ src/app/list-user/list-user.component.spec.ts
@@ -1,24 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ListUserComponent } from './list-user.component';
+// import { ListUserComponent } from './list-user.component';
 
-describe('ListUserComponent', () => {
-  let component: ListUserComponent;
-  let fixture: ComponentFixture<ListUserComponent>;
+// describe('ListUserComponent', () => {
+//   let component: ListUserComponent;
+//   let fixture: ComponentFixture<ListUserComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ListUserComponent],
-    }).compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ListUserComponent],
+//     }).compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ListUserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(ListUserComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
