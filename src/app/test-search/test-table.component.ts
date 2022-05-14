6381fd23d67460034c1305b15db2b6b902ad989b
hoanganhhththt <66316469+hoanganhhththt@users.noreply.github.com>
2022-05-14 14:52:14 +0700

    refactor/change layoutt table

--- src/app/test-search/test-table.component.ts
+++ src/app/test-search/test-table.component.ts
@@ -3,6 +3,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
+export interface Data {
+  id: number;
+  status: string;
+  name: string;
+  typeNow: string;
+  accountNumber: string;
+  accountName: string;
+  bank: number;
+  errorDetail: string;
+  description: string;
+  isRequest: number;
+  phoneNumber: string;
+  disabled: boolean;
+}
 @Component({
   selector: 'app-test-search',
   templateUrl: './test-table.component.html',
@@ -19,7 +33,15 @@ export class TestSearch implements OnInit {
       disabled: false,
     },
   ];
-  checked = true;
+  // checked = true;
+  checked = false;
+  loading = false;
+  indeterminate = false;
+  listOfData: readonly Data[] = [];
+  listOfCurrentPageData: readonly Data[] = [];
+  setOfCheckedId = new Set<number>();
+  isVisible = false;
+  listOfDisplayData = [...this.listOfData];
 
   // submitForm(): void {
   //   console.log('submit', this.validateForm.value);
@@ -34,11 +56,26 @@ export class TestSearch implements OnInit {
       isdnFile: [null],
       successFile: [null],
       cancelTime: [null],
-      shope: [null],
+      shop: [null],
       cancelFalse: [false],
       cancelKK: [true],
       cancelUAC: [true],
     });
+    this.listOfData = new Array(100).fill(0).map((_, index) => ({
+      id: index,
+      status: 'Hoạt động',
+      name: `Edward King ${index}`,
+      typeNow: 'Text',
+      accountNumber: '184323256',
+      accountName: 'Hoàng Anh',
+      bank: 19424,
+      phoneNumber: '0911469899',
+      errorDetail: 'Lỗi số 1',
+      description: 'Vì lỗi số 1',
+      isRequest: index % 2,
+      disabled: index % 2 === 1,
+    }));
+    this.listOfDisplayData = [...this.listOfData];
   }
 
   resetForm(): void {
@@ -53,5 +90,39 @@ export class TestSearch implements OnInit {
     } else if (info.file.status === 'error') {
       this.msg.error(`${info.file.name} file upload failed.`);
     }
+  }
+  onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
+    this.listOfCurrentPageData = listOfCurrentPageData;
+    this.refreshCheckedStatus();
+  }
+  refreshCheckedStatus(): void {
+    const listOfEnabledData = this.listOfCurrentPageData.filter(({ disabled }) => !disabled);
+    this.checked = listOfEnabledData.every(({ id }) => this.setOfCheckedId.has(id));
+    this.indeterminate = listOfEnabledData.some(({ id }) => this.setOfCheckedId.has(id)) && !this.checked;
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+
+  onAllChecked(checked: boolean): void {
+    this.listOfCurrentPageData
+      .filter(({ disabled }) => !disabled)
+      .forEach(({ id }) => this.updateCheckedSet(id, checked));
+    this.refreshCheckedStatus();
+  }
+  updateCheckedSet(id: number, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+  searchStatus(e: Event): void {
+    console.log((e.target as HTMLInputElement).value);
+    this.listOfDisplayData = this.listOfData.filter(
+      (item: Data) => item.status.indexOf((e.target as HTMLInputElement).value) !== -1
+    );
   }
 }
